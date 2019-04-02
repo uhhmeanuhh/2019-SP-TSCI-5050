21468b26c300deccaa84cb4323d6e4d60f0b6020
Alex Bokov <bokov@uthscsa.edu>
2019-04-02 16:00:18 -0500

    More cleanup, tweaks to TEMPLATE.R

--- MyProject/TEMPLATE.R
+++ MyProject/TEMPLATE.R
@@ -9,13 +9,14 @@
 # %1$s %2$s %3$s %4$s %5$s %6$s
 .packages <- %4$s;
 .deps <- %6$s;
-.loadedobjects <- c();
 debug <- 0;
 if(debug>0) source('global.R') else {
   .junk<-capture.output(source('global.R',echo=F))};
 if(length(.packages) > 1 || .packages != '') instrequire(.packages);
 .currentscript <- "%5$s";
 tself(scriptname=.currentscript);
+.origfiles <- ls();
+.loadedobjects <- c();
 if(length(.deps)>1 || .deps != ''){
   for(ii in .deps){
     .depdata <- paste0(ii,'.rdata');
@@ -24,17 +25,42 @@ if(length(.deps)>1 || .deps != ''){
     .loadedobjects <- union(.loadedobjects,tload(.depdata));
   }
 }
-#' ### Put your code here!
 #+ echo=F
-pander(summary(dat0));
+#############################################################
+# Your code goes below, content provided only as an example #
+#############################################################
+
+#' ### Data Characterization
+#' 
+#' Quality control, descriptive statistics, etc.
 
 #+ echo=F
-# Saving original file-list so we don't keep exporting functions and 
-# environment variables to other scripts
-.origfiles <- ls();
+# characterization ----
+pander(summary(dat0)); 
+
+#' ### Data Analysis
+#' 
+#' Fitting the actual statistical models.
+#+ echo=F
+# analysis ----
 
 #+ echo=F
+#############################################################
+# End of your code, start of boilerplate code               #
+#############################################################
+
 # save out with audit trail ----
-tsave(file=paste0(.currentscript,'.rdata'),list=setdiff(ls(),.origfiles),verbose=F);
+# Saving original file-list so we don't keep exporting functions and 
+# environment variables to other scripts. Could also replace .origfiles
+# with the expression c(.origfiles,.loadedobjects) and then, nothing
+# get inherited from previous files, only the stuff created in this 
+# script gets saved. If this doesn't make sense, don't worry about it.
+tsave(file=paste0(.currentscript,'.rdata'),list=setdiff(ls(),.origfiles)
+      ,verbose=F);
+
+#' ### Audit Trail
+#+ echo=F
+.wt <- walktrail();
+pander(.wt[order(.wt$sequence),-5],split.tables=Inf,justify='left',missing='');
 #+ echo=F,eval=F
 c()
